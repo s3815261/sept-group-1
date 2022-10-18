c37c3a1ce21158d14c0207840c27bffc9da32247
marcus <mmxo5813@gmail.com>
2022-10-18 21:21:09 +1100

    cleanup

--- backend-microservices/gateway/src/main/java/com/sept/peopleservice/config/SpringCloudGatewayRouting.java
+++ backend-microservices/gateway/src/main/java/com/sept/peopleservice/config/SpringCloudGatewayRouting.java
@@ -9,21 +9,10 @@ import org.springframework.context.annotation.Configuration;
 public class SpringCloudGatewayRouting {
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("users", r->r.path("/users/**").uri("http://localhost:8081")) //static routing
-//                .route("orderId", r->r.path("/order/**").uri("lb://ORDER-SERVICE")) //dynamic routing
-//                .build();
-
         return builder.routes()
-                .route("user-service", r -> r.path("/users/**").uri("lb://USER-SERVICE")).build();
-//                .route(p -> p
-//                        .path("/users/**")
-//                        .uri("http://localhost:8081"))
-//                .build();
-//        return builder.routes()
-//                .route(p -> p
-//                        .path("/users/**")
-//                        .uri("http://httpbin.org:81"))
-//                .build();
+                .route("user-service", r -> r.path("/users/**").uri("lb://USER-SERVICE"))
+                .route("doctor-service", r -> r.path("/doctor/**").uri("lb://DOCTOR-SERVICE"))
+                .route("appointment-service", r -> r.path("/appointment/**").uri("lb://APPOINTMENT-SERVICE"))
+                .build();
     }
 }
